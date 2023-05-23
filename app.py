bb3359d9ad92dc6109c87f1793c1346ef3959429
vikrantc672 <vikrantc083@gmail.com>
2023-05-24 00:21:58 +0530

    Emotion Recognition in real Time complete

--- app.py
+++ app.py
@@ -41,7 +41,28 @@ def detect_emotion():
      
     return render_template("result.html",value=result[0]['dominant_emotion'])
 
-
+@app.route('/cam_emotion')
+def cam_emotion():
+    cam =cv2.VideoCapture(0)
+    cv2.namedWindow("Screenshot Capture")
+    img_counter=0
+    while True:
+        ret,frame=cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        cv2.imshow("test",frame)
+        k = cv2.waitKey(1)
+        if k%256==27:
+            print("Escape Key Pressed")
+            break
+        elif k%256==32:
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
+    cam.release()
+    cv2.destroyAllWindows()
 if __name__ == '__main__':
     app.run(port=5000, debug=True) 
 # run the application in debug mode
